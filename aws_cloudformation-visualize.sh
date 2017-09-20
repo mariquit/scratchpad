f00ec965aab9e57917391f9828eef47f68c1cb5b
johnmariquit <32053448+johnmariquit@users.noreply.github.com>
2017-09-20 17:16:41 +0800

    Added visualization tool of CloudFormation scripts

---
+++ aws_cloudformation-visualize.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# usage: make a soft link in your PATH to this file: 
+#     ln -s ~/git/scratchpad/aws_cloudformation-visualize.sh ~/bin/cfn-visualize
+#
+# note: you will also need to do the same to the cfviz.py.  See cfviz.py for 
+#
+# prerequisites: graphviz + imagemagick
+
+[ -n "$1" ] || exit 1
+[ -f "$1" ] && cat "$1" | cfviz | dot -Tsvg -o/tmp/output.svg
+[ -f "/tmp/output.svg" ] && convert /tmp/output.svg "$1".png && rm -f /tmp/output.svg
