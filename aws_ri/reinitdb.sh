c19d49985e53662f06961d78b199d607130cd042
johnmariquit <32053448+johnmariquit@users.noreply.github.com>
2017-10-13 13:21:44 +0800

    updated code

--- aws_ri/reinitdb.sh
+++ aws_ri/reinitdb.sh
@@ -5,7 +5,7 @@ function dbexec {
   psql -d dbr -f $1 -q
 }
 
-if [ -e "../aws_billing/reinitdb.sh" ] 
+if [ -e "../aws_billing/reinitdb.sh" ]
 then
   cd ../aws_billing
   ./reinitdb.sh
@@ -16,3 +16,4 @@ echo "\033[32m$PWD\033[0m"
 dbexec 5.pricing.ec2.sql
 dbexec 5.pricing.rds.sql
 dbexec 6.ri_analysis.sql
+[ -e "6.ri_analysis.sql.do_not_push" ] && dbexec 6.ri_analysis.sql.do_not_push
